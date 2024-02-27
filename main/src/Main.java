d313f8076be7a4187b2a1fb4fcb0a987a16547df
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-02-27 14:40:52 +0100

    init commit

--- main/src/Main.java
+++ main/src/Main.java
@@ -10,14 +10,21 @@ public class Main {
 
     Ingredient tomato = new Ingredient("Tomato", 100);
     Ingredient cheese = new Ingredient("Cheese", 50);
+    Ingredient mushroom = new Ingredient("Mushroom", 70);
 
-   /*
-    if(ingredientDao.insertIngredient(tomato)){
+
+    mushroom =  ingredientDao.insertIngredient(mushroom);
+    if(mushroom != null)  System.out.println("Mushroom ingredient inserted successfully.");
+    System.out.println(mushroom);
+
+
+    tomato = ingredientDao.insertIngredient(tomato);
+    if( tomato != null ){
       System.out.println("Tomato ingredient inserted successfully.");
     }
 
-
-    if(ingredientDao.insertIngredient(cheese)){
+    cheese = ingredientDao.insertIngredient(cheese);
+    if( cheese != null ){
       System.out.println("Cheese ingredient inserted successfully.");
     }
 
@@ -30,11 +37,18 @@ public class Main {
 
     // Update an ingredient
     System.out.println("Update Tomato quantity");
-    tomato = ingredientDao.getIngredient(1);
-    tomato.setQuantity(50);
+
+    if ( tomato != null ) tomato.setQuantity(50);
     if(ingredientDao.updateIngredient(tomato)){
       System.out.println("Tomato ingredient quantity updated successfully!");
     }
-*/
+
+    // Delete an ingredient
+    System.out.println("Delete Tomato ingredient");
+    if ( tomato != null ){
+      if(ingredientDao.deleteIngredient(tomato.getId())){
+      System.out.println("Tomato is deleted");
+    }}
+
   }
 }
