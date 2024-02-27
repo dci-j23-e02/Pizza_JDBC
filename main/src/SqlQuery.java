1fb2a338c73c7a96b8a1eddd3cc22697e3ed0dfd
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-02-27 10:40:31 +0100

    init commit

--- main/src/SqlQuery.java
+++ main/src/SqlQuery.java
@@ -1,4 +1,19 @@
 package src;
 
 public enum SqlQuery {
+  GET_INGREDIENT_BY_ID(""),
+  GET_ALL_INGREDIENTS(""),
+  INSERT_INGREDIENTS(""),
+  UPDATE_INGREDIENT(""),
+  DELETE_INGREDIENT("");
+
+  private final String query;
+
+  SqlQuery(String query) {
+    this.query = query;
+  }
+
+  public String getQuery() {
+    return query;
+  }
 }
